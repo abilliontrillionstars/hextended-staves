762292b137ef1dee561039bd0a00f9745182bfd9
Laniakea <billiontrillionstars@gmail.com>
2025-07-27 23:18:38 -0400

    fix import & add unique method

--- common/src/main/java/abilliontrillionstars/hextended/mixin/AccessorMsgShiftScrollC2S.java
+++ common/src/main/java/abilliontrillionstars/hextended/mixin/AccessorMsgShiftScrollC2S.java
@@ -1,10 +1,10 @@
 package abilliontrillionstars.hextended.mixin;
 
 import at.petrak.hexcasting.common.msgs.MsgShiftScrollC2S;
-import dev.architectury.patchedmixin.staticmixin.spongepowered.asm.mixin.Mixin;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(MsgShiftScrollC2S.class)
--- common/src/main/java/abilliontrillionstars/hextended/mixin/MsgShiftScrollMixin.java
+++ common/src/main/java/abilliontrillionstars/hextended/mixin/MsgShiftScrollMixin.java
@@ -6,7 +6,10 @@ import at.petrak.hexcasting.common.msgs.MsgShiftScrollC2S;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,19 +18,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MsgShiftScrollC2S.class)
-public class MsgShiftScrollMixin
+public abstract class MsgShiftScrollMixin
 {
+    @Shadow protected abstract void spellbook(ServerPlayer sender, InteractionHand hand, ItemStack stack, double delta);
+
     @Inject(method = "handleForHand", at = @At(value = "HEAD"), remap = false)
     private void handleForHand(ServerPlayer sender, InteractionHand hand, double delta, CallbackInfo ci)
     {
         if (delta != 0)
         {
             var stack = sender.getItemInHand(hand);
-
-            if(stack.getItem() == HexItems.SPELLBOOK)
-            {
-                //spellbook(sender, hand, stack, delta);
-            }
+            if(stack.getItem() == LanisHextendedStavesItems.BOUND_SPELLBOOK_TEST)
+                hextended$spellbook(sender, hand, stack, delta);
         }
+    }
+
+
+    @Unique
+    void hextended$spellbook(ServerPlayer sender, InteractionHand hand, ItemStack stack, double delta)
+    {
+        this.spellbook(sender, hand, stack, delta);
     }
 }
