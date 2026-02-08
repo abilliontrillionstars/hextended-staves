4ed8173a7bfbeab01e78c61beac770d9785d41fc
Laniakea <billiontrillionstars@gmail.com>
2026-02-07 22:28:35 -0500

    switch to fabric template. kaboom!

--- common/src/main/java/abilliontrillionstars/hextended/items/bookbinding/ItemSpellbookCover.java
+++
@@ -1,8 +0,0 @@
-package abilliontrillionstars.hextended.items.bookbinding;
-
-import net.minecraft.world.item.Item;
-
-public class ItemSpellbookCover extends Item
-{
-    public ItemSpellbookCover(Properties properties) { super(properties); }
-}
---
+++ src/client/java/abilliontrillionstars/HextendedStavesDataGenerator.java
@@ -0,0 +1,11 @@
+package abilliontrillionstars;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HextendedStavesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
